9bdd2726375a340c256ca345d3103e7429a21805
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-05-16 23:52:20 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Minutes_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time, mins, hours;
+
+            Console.Write("Enter the time in minutes: ");
+            time = Int32.Parse(Console.ReadLine());
+
+            hours = (int)(time / 60);
+            mins = time % 60;
+
+            Console.WriteLine(hours + " hours " + mins + " minutes");
+        }
+    }
+}
